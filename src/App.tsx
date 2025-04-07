d3a41822ec09c37e606454846d62d629b4302c22
Nihad Ismayilzada <nismayilzada@theindoorlab.com>
2025-04-06 17:39:57 -0700

    adding-materials-dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { OrbitControls } from '@react-three/drei';
 import './App.css'
 import Marker from './components/Marker';
 import { NumberVec3 } from './models/number-vector3-matrix';
+import * as THREE from 'three';
 
 const matrix: NumberVec3[] = [
   [0, 0, 0],
@@ -11,6 +12,14 @@ const matrix: NumberVec3[] = [
   [0, 0, 4],
 ];
 
+const alertMaterial = new THREE.MeshStandardMaterial({ color: 'red' });
+const realisticMaterial = new THREE.MeshStandardMaterial({
+  color: 'green',         // The base color of the material
+  roughness: 0.4,       // Adjust roughness (lower for smoother surfaces, higher for rougher)
+  metalness: 0.2,       // Adjust metalness (0 = non-metallic, 1 = metallic)
+  emissive: new THREE.Color(0.1, 0.1, 0.1), // Simulate glowing effect
+  emissiveIntensity: 0.5, // Intensity of the emissive glow
+});
 function App() {
 
   return (
@@ -19,7 +28,7 @@ function App() {
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       {matrix.map((pos, i) => (
-        <Marker key={i} position={pos} />
+        <Marker key={i} position={pos} material={ i % 2 === 0 ? alertMaterial : realisticMaterial}/>
       ))}
       <OrbitControls />
     </Canvas>
--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -1,22 +1,23 @@
 import { Vector3 } from '@react-three/fiber';
+import * as THREE from 'three';
 
 type MarkerPositionProp = {
     position: [number, number, number] | Vector3;
+    material: THREE.Material;
   };
 
-function Marker({position}: MarkerPositionProp) {
+
+function Marker({position, material}: MarkerPositionProp) {
     return (
         <group position={position}>
           {/* The circular base of the marker */}
-          <mesh position={[0, 0, 0]}>
+          <mesh position={[0, 0, 0]} material={material}>
             <sphereGeometry args={[0.5, 32, 32]} />
-            <meshStandardMaterial color="red" />
           </mesh>
     
           {/* The pointed tip of the marker */}
-          <mesh position={[0, -0.75, 0]}>
+          <mesh position={[0, -0.75, 0]} material={material}>
             <coneGeometry args={[0.2, 1.5, 32]} />
-            <meshStandardMaterial color="red" />
           </mesh>
         </group>
       );
